f485f1f95d5fd444834cf2b5a6e1d42eeaaf5d77
Nguyen Duy <146707420+duy-nq@users.noreply.github.com>
2024-11-08 00:34:41 +0700

    feat: new UI with function - create a new navbar - reallocate codes for sending pics to new fragment

---
+++ app/src/main/java/com/example/diseasesdetection/BasicFragment.kt
@@ -0,0 +1,172 @@
+package com.example.diseasesdetection
+
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.DataOutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+class BasicFragment : Fragment() {
+    private lateinit var buttonTakePhoto: Button
+    private lateinit var buttonChooseFromGallery: Button
+    private lateinit var buttonSendImage: Button
+    private lateinit var imageView: ImageView
+    private lateinit var progressBar: ProgressBar
+
+    private var imageName: String = ""
+    private var imageUri: Uri? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.basic_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonTakePhoto = view.findViewById(R.id.button_take_photo)
+        buttonChooseFromGallery = view.findViewById(R.id.button_choose_from_gallery)
+        buttonSendImage = view.findViewById(R.id.button_send_image)
+        imageView = view.findViewById(R.id.image_view)
+        progressBar = view.findViewById(R.id.progressBar)
+
+        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                try {
+                    imageUri = result.data?.data
+                    if (imageUri != null) {
+                        imageView.setImageURI(imageUri)
+                        val imgArray = imageUri.toString().split("/").toTypedArray()
+                        imageName = imgArray[imgArray.size-1]
+                        Toast.makeText(requireContext(), imageUri.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+                catch (e: Exception) {
+                    println("Caught an InsufficientFundsException: ${e.message}")
+                }
+            }
+        }
+
+        imageView.setOnClickListener {
+            val intent = Intent(requireContext(), FullScreenImageActivity::class.java)
+            intent.putExtra("imageUri", imageUri)
+            startActivity(intent)
+        }
+
+        buttonTakePhoto.setOnClickListener {
+            Toast.makeText(requireContext(), "Take Photo feature is under construction.", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonChooseFromGallery.setOnClickListener {
+            val getPictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            resultLauncher.launch(getPictureIntent)
+        }
+
+        buttonSendImage.setOnClickListener {
+            // Base 64 Encode
+            val encodedFile: String
+            val inputStream = requireContext().contentResolver.openInputStream(imageUri!!)
+            val bytes = inputStream?.readBytes()
+            inputStream?.close()
+
+            if (bytes != null) {
+                encodedFile = Base64.encodeToString(bytes, Base64.NO_WRAP)
+            }
+            else {
+                throw Exception("Fail!")
+            }
+
+            val uploadURL =
+                "https://detect.roboflow.com/deeper-diseases/1?api_key=LfVTZkyLWVh9dR2GzXjc&confidence=20&overlap=30&format=image&stroke=2&labels=true"
+
+            CoroutineScope(Dispatchers.IO).launch {
+                var connection: HttpURLConnection? = null
+
+                withContext(Dispatchers.Main) {
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                try {
+                    // Configure connection to URL
+                    val url = URL(uploadURL)
+                    connection = url.openConnection() as HttpURLConnection
+                    connection.requestMethod = "POST"
+                    connection.setRequestProperty("Content-Type",
+                        "application/x-www-form-urlencoded")
+                    connection.setRequestProperty("Content-Length",
+                        encodedFile.toByteArray().size.toString()
+                    )
+                    connection.setRequestProperty("Content-Language", "en-US")
+                    connection.useCaches = false
+                    connection.doOutput = true
+
+                    //Send request
+                    val wr = DataOutputStream(
+                        connection.outputStream)
+                    wr.writeBytes(encodedFile)
+                    wr.close()
+
+                    // Get Response
+//                    val stream = connection.inputStream
+//                    val reader = BufferedReader(InputStreamReader(stream))
+//                    var line: String?
+//                    while (reader.readLine().also { line = it } != null) {
+//                        line?.let { it1 -> Log.d("LINE", it1) }
+//                    }
+//                    reader.close()
+
+                    // Get Image
+                    if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                        val contentType = connection.getHeaderField("Content-Type")
+                        if (contentType == "image/jpeg") {
+                            // Get InputStream and decode to Bitmap
+                            val resImg = connection.inputStream
+                            val bitmap = BitmapFactory.decodeStream(resImg)
+
+                            withContext(Dispatchers.Main) {
+                                imageView.setImageBitmap(bitmap) // Display the image in your ImageView
+                            }
+
+                            inputStream.close()
+                        } else {
+                            Log.e("Error", "Unexpected content type: $contentType")
+                        }
+                    } else {
+                        Log.e("Error", "HTTP error: ${connection.responseCode}")
+                    }
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    withContext(Dispatchers.Main) {
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), "Process is now done!", Toast.LENGTH_SHORT).show()
+                    }
+                    connection?.disconnect()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/diseasesdetection/MainActivity.kt
+++ app/src/main/java/com/example/diseasesdetection/MainActivity.kt
@@ -1,145 +1,33 @@
 package com.example.diseasesdetection
 
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.result.contract.ActivityResultContracts
-import java.io.*
-import java.net.HttpURLConnection
-import java.net.URL
-import android.util.Base64
-import android.util.Log
-import kotlinx.coroutines.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
-    private lateinit var buttonTakePhoto: Button
-    private lateinit var buttonChooseFromGallery: Button
-    private lateinit var buttonSendImage: Button
-    private lateinit var imageView: ImageView
-
-    private var imageName: String = ""
-    private var imageUri: Uri? = null
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.basic_layout)
+        setContentView(R.layout.advanced_layout)
 
-        buttonTakePhoto = findViewById(R.id.button_take_photo)
-        buttonChooseFromGallery = findViewById(R.id.button_choose_from_gallery)
-        buttonSendImage = findViewById(R.id.button_send_image)
-        imageView = findViewById(R.id.image_view)
+        val basicLayout = BasicFragment()
+        setCurrentFragment(basicLayout)
 
-        val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                try {
-                    imageUri = result.data?.data
-                    if (imageUri != null) {
-                        imageView.setImageURI(imageUri)
-                        val imgArray = imageUri.toString().split("/").toTypedArray()
-                        imageName = imgArray[imgArray.size-1]
-                        Toast.makeText(this, imageUri.toString(), Toast.LENGTH_SHORT).show()
-                    }
-                }
-                catch (e: Exception) {
-                    println("Caught an InsufficientFundsException: ${e.message}")
-                }
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.basic -> setCurrentFragment(basicLayout)
             }
+            true
         }
+    }
 
-        imageView.setOnClickListener {
-            val intent = Intent(this, FullScreenImageActivity::class.java)
-            intent.putExtra("imageUri", imageUri)
-            startActivity(intent)
-        }
-
-        buttonTakePhoto.setOnClickListener {
-            Toast.makeText(this, "Take Photo feature is under construction.", Toast.LENGTH_SHORT).show()
-        }
-
-        buttonChooseFromGallery.setOnClickListener {
-            val getPictureIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            resultLauncher.launch(getPictureIntent)
-        }
-
-        buttonSendImage.setOnClickListener {
-            // Base 64 Encode
-            val encodedFile: String
-            val inputStream = contentResolver.openInputStream(imageUri!!)
-            val bytes = inputStream?.readBytes()
-            inputStream?.close()
-
-            if (bytes != null) {
-                encodedFile = Base64.encodeToString(bytes, Base64.NO_WRAP)
-            }
-            else {
-                throw Exception("Fail!")
-            }
-
-            val uploadURL =
-                "https://detect.roboflow.com/deeper-diseases/1?api_key=LfVTZkyLWVh9dR2GzXjc&confidence=10&overlap=10&format=image"
-
-            CoroutineScope(Dispatchers.IO).launch {
-                    var connection: HttpURLConnection? = null
-                    try {
-                    // Configure connection to URL
-                    val url = URL(uploadURL)
-                    connection = url.openConnection() as HttpURLConnection
-                    connection.requestMethod = "POST"
-                    connection.setRequestProperty("Content-Type",
-                        "application/x-www-form-urlencoded")
-                    connection.setRequestProperty("Content-Length",
-                        encodedFile.toByteArray().size.toString()
-                    )
-                    connection.setRequestProperty("Content-Language", "en-US")
-                    connection.useCaches = false
-                    connection.doOutput = true
-
-                    //Send request
-                    val wr = DataOutputStream(
-                        connection.outputStream)
-                    wr.writeBytes(encodedFile)
-                    wr.close()
-
-                    // Get Response
-//                    val stream = connection.inputStream
-//                    val reader = BufferedReader(InputStreamReader(stream))
-//                    var line: String?
-//                    while (reader.readLine().also { line = it } != null) {
-//                        line?.let { it1 -> Log.d("LINE", it1) }
-//                    }
-//                    reader.close()
-                        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
-                            val contentType = connection.getHeaderField("Content-Type")
-                            if (contentType == "image/jpeg") {
-                                // Get InputStream and decode to Bitmap
-                                val resImg = connection.inputStream
-                                val bitmap = BitmapFactory.decodeStream(resImg)
-
-                                withContext(Dispatchers.Main) {
-                                    imageView.setImageBitmap(bitmap) // Display the image in your ImageView
-                                }
-
-                                inputStream.close()
-                            } else {
-                                Log.e("Error", "Unexpected content type: $contentType")
-                            }
-                        } else {
-                            Log.e("Error", "HTTP error: ${connection.responseCode}")
-                        }
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                } finally {
-                        connection?.disconnect()
-                }
-            }
-
-
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
         }
     }
 }
