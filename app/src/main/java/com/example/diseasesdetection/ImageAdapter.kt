538afc27d4f8b1c0c37f8a039e8b62ef914dd786
Nguyen Duy <146707420+duy-nq@users.noreply.github.com>
2024-11-14 01:23:21 +0700

    refactor: remove unused import

--- app/src/main/java/com/example/diseasesdetection/ImageAdapter.kt
+++ app/src/main/java/com/example/diseasesdetection/ImageAdapter.kt
@@ -1,15 +1,12 @@
 package com.example.diseasesdetection
 
 import android.net.Uri
-import android.provider.Settings.Global.getString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.withContext
 
 class ImageAdapter(
     private val imageUris: List<Uri>,
